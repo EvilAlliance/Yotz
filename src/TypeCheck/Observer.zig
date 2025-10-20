6a842a5a5dddddcadd222fe69ea976b458836f6b
EvilAlliance <chialanza17@gmail.com>
2025-10-20 16:50:40 -0300

    Observer

--- src/TypeCheck/Observer.zig
+++ src/TypeCheck/Observer.zig
@@ -1,21 +1,61 @@
-fn Observer(key: type) type {
+pub fn Observer(Key: type, Args: type) type {
     return struct {
+        const Self = @This();
         const Handler = struct {
-            func: fn (anyopaque) anyerror!void,
-            args: anyopaque,
-
-            pub fn call(self: Handler) void {
-                // Call func with args using @call and catch errors inside
-                return @call(.auto, self.func, self.args);
-            }
+            func: fn (Args) void,
+            args: Args,
+            node: std.SinglyLinkedList.Node,
         };
 
-        eventToFunc: std.AutoHashMap(
-            key,
-            Handler,
+        listHandler: std.ArrayList(*Handler),
+
+        pool: *std.Thread.Pool,
+        mutex: std.Thread.Mutex = .{},
+
+        eventToFunc: std.AutoHashMapUnmanaged(
+            Key,
+            std.SinglyLinkedList,
         ),
+
+        // PERF: Later check if listHandler is worth it
+        pub fn init(alloc: Allocator, pool: *std.Thread.Pool) Self {
+            return .{
+                .listHandler = .initCapacity(alloc, std.math.pow(2, 7)),
+
+                .pool = pool,
+
+                .eventToFunc = .init(),
+            };
+        }
+
+        pub fn push(self: *Self, alloc: Allocator, wait: Key, func: fn (Args) void, args: Args) Allocator.Error!void {
+            self.mutex.lock();
+            defer self.mutex.unlock();
+
+            const handler: *Handler = if (self.listHandler.getLastOrNull()) |handlerOld| handlerOld else alloc.create(Handler);
+
+            handler.* = .{
+                .func = func,
+                .args = args,
+            };
+
+            if (self.eventToFunc.getPtr(wait)) |node| {
+                node.prepend(&handler.node);
+            } else {
+                var link = std.SinglyLinkedList{};
+
+                link.prepend(&handler.node);
+                try self.eventToFunc.put(alloc, wait, link);
+            }
+        }
+
+        pub fn deinit(self: *Self, alloc: Allocator) void {
+            self.eventToFunc.deinit(alloc);
+            self.listHandler.deinit(alloc);
+        }
     };
 }
 
 const std = @import("std");
 
+const Allocator = std.mem.Allocator;
